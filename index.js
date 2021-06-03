dc2108e692456b7f70293c1f43bbcd91cad0de8d
Bespaliy <55046429+Bespaliy@users.noreply.github.com>
2021-06-03 16:51:37 +0300

    course model

--- index.js
+++ index.js
@@ -2,33 +2,29 @@
 
 const http = require('http');
 const staticServer = require('./static.js');
-const User = require('./Models/User.js');
+const route = require('./src/route/routing.js');
+
 const PORT = 8001;
 
 staticServer(8000);
 
-const receiveArgs = async (req) => { 
-  	const buffers = [];
-  	for await (const chunk of req) buffers.push(chunk);
-  	const data = Buffer.concat(buffers).toString();
-  	if (!data) return;
-  	return JSON.parse(data);
+const receiveArgs = async req => { 
+  const buffers = [];
+  for await (const chunk of req) buffers.push(chunk);
+  const data = Buffer.concat(buffers).toString();
+  if (!data) return;
+  return JSON.parse(data);
 };
 
 
 http.createServer(async (req, res) => {
 	const url = req.url;
-	try {
-		const args = await receiveArgs(req);
-		if (args) {
-			const user = new User(...Object.values(args));
-			await User.save(user);
-		} 
-	} catch(e) {
-		console.log(e);
+	const args = await receiveArgs(req);
+	if (args) {
+		route[url](args);
 	}
 	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
 	res.setHeader('Access-Control-Allow-Methods', 'POST');
 	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-	res.end();	
+	res.end();
 }).listen(PORT, console.log(`Server on port ${PORT}`));
--- static.js
+++ static.js
@@ -4,11 +4,12 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+
 module.exports = port => {
 	http.createServer(async (req, res) => {
 		const url = '/' === req.url  ? 'index' : req.url.slice(1);
 		const fileExt = path.extname(url);
-		const fileName = !fileExt ? url.concat('.html') : url; 
+		const fileName = !fileExt ? url.concat('.html') : url;
 		try {
 			fs.createReadStream(path.join(__dirname, 'static', fileName)).pipe(res);
 		} catch(e) {
@@ -16,7 +17,5 @@ module.exports = port => {
 			res.statusMessage = 404;
 			res.end('<h1>File not found</h1>');
 		}
-	}).listen(port);
-
-	console.log(`Static on port ${port}`);
+	}).listen(port, console.log(`Static on port ${port}`));
 };
